9d0a2643d1cadb7cd38358e654b0ea030ab66add
Ram Bhagat Thakur <103376638+ram-bhagat-thakur@users.noreply.github.com>
2025-06-04 23:33:18 +0530

    Update script.js
    Removed extra code

--- public/script.js
+++ public/script.js
@@ -1,39 +1,3 @@
-document.querySelectorAll(".checkbox").forEach(checkbox => {
-    checkbox.addEventListener("click", function () {
-        var text = this.nextElementSibling; // Get the corresponding text element
-        text.style.textDecoration = this.checked ? "line-through" : "none";
-    });
-});
-document.querySelectorAll(".delete").forEach(deleteButton => {
-    deleteButton.addEventListener("click", function () {
-        var todoItem = this.closest("li");
-        todoItem.remove();
-    });
-
-});
-
-
-document.querySelectorAll(".edit").forEach(editButton => {
-    editButton.addEventListener("click", function () {
-        var todoItem = this.closest("li");
-        var textElement = todoItem.querySelector("#update");
-        var checkbox = todoItem.querySelector(".checkbox");
-        var checkText = todoItem.querySelector("h4"); // Get the correct text element
-
-        if (this.innerText === "Edit") {
-            this.innerText = "Save";
-            textElement.readOnly = false;
-            textElement.focus();
-            checkbox.disabled = true; // Disable the checkbox while editing
-        } else {
-            this.innerText = "Edit";
-            textElement.readOnly = true;
-            checkbox.disabled = false; // Enable the checkbox after editing
-            checkText.innerText = textElement.value; // Update the h4 text with the new value
-        }
-    });
-});
-
 document.querySelectorAll(".filter a").forEach(filterLink => {
     filterLink.addEventListener("click", function (event) {
         event.preventDefault();
