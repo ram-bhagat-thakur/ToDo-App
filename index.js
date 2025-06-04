ce7c85288c90d681034f6e504c2a3cd5eb1b770f
Ram Bhagat Thakur <103376638+ram-bhagat-thakur@users.noreply.github.com>
2025-06-04 23:30:32 +0530

    Update index.js
    Updated for updating and deleting tasks.

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const express = require("express");
 const bodyParser=require("body-parser");
-var app=express();
+const app=express();
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 
-var items=[];
-
+const items=[];
+let id=0;
 app.get("/",function(req,res){
-    //console.log(items);
     res.render("list",{ejes:items});
 });
 
-app.post("/",function(req,res){
-    var item=req.body;
-    console.log(item);
+app.post("/", function(req, res) {
+    const item = {
+        id: id++,  
+        todo: req.body.todo,
+        priority: req.body.priority
+    };
     items.push(item);
     res.redirect("/");
-})
+});
+
+
+app.post("/delete", function(req, res) {
+    const itemId = parseInt(req.body.id);
+    const index = items.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+        items.splice(index, 1);
+    }
+    res.redirect("/");
+});
+
+
+app.post("/update", function(req, res) {
+    const itemId = parseInt(req.body.id);
+    const index = items.findIndex(item => item.id === itemId);
+    if (index !== -1) {
+        items[index].todo = req.body.todo;
+        items[index].priority = req.body.priority;
+    }
+    res.redirect("/");
+});
+
 
 
 
